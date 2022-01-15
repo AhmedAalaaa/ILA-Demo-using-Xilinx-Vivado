77c7498e8f6e954ca8c72d5ddf2b8775c7a3257c
Ahmed Alaa <AhmedAlaaAbdElWahhab@gmail.com>
2022-01-15 19:05:36 +0200

    create the vivado project

---
+++ ILA_demo/ILA_demo.srcs/sources_1/bd/design_1/ip/design_1_ila_0_0/design_1_ila_0_0_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+// Date        : Sat Jan 15 18:36:44 2022
+// Host        : DESKTOP-A2OCTUJ running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/workset/ILA_demo/ILA_demo.srcs/sources_1/bd/design_1/ip/design_1_ila_0_0/design_1_ila_0_0_stub.v
+// Design      : design_1_ila_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "ila,Vivado 2019.1" *)
+module design_1_ila_0_0(clk, probe0)
+/* synthesis syn_black_box black_box_pad_pin="clk,probe0[7:0]" */;
+  input clk;
+  input [7:0]probe0;
+endmodule
---
+++ ILA_demo/ILA_demo.srcs/sources_1/bd/design_1/ip/design_1_ila_0_0/design_1_ila_0_0_stub.vhdl
@@ -0,0 +1,31 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+-- Date        : Sat Jan 15 18:36:44 2022
+-- Host        : DESKTOP-A2OCTUJ running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/workset/ILA_demo/ILA_demo.srcs/sources_1/bd/design_1/ip/design_1_ila_0_0/design_1_ila_0_0_stub.vhdl
+-- Design      : design_1_ila_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity design_1_ila_0_0 is
+  Port ( 
+    clk : in STD_LOGIC;
+    probe0 : in STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+
+end design_1_ila_0_0;
+
+architecture stub of design_1_ila_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,probe0[7:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "ila,Vivado 2019.1";
+begin
+end;
